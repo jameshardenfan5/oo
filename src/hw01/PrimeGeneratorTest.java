20b2e334792c79a05a373884e2db23643eea7885
jameshardenfan5 <ajg2157@caa.columbia.edu>
2020-09-26 23:01:53 -0400

    Update PrimeGeneratorTest.java

--- src/hw01/PrimeGeneratorTest.java
+++ src/hw01/PrimeGeneratorTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
-import java.util.Arrays;
 
 public class PrimeGeneratorTest {
     @Test
@@ -41,25 +40,22 @@ public class PrimeGeneratorTest {
     @Test
     public void generatePrimesEighttoNine() {
         PrimeGenerator gen = new PrimeGenerator(8, 9);
-        LinkedList<Long> expected=new LinkedList<Long>();
+        Long[] expected={};
         gen.generatePrimes();
-        LinkedList<Long> actual = gen.getPrimes();
-        assertEquals(expected, actual);
+        assertArrayEquals(expected, gen.getPrimes().toArray());
     }
     @Test
     public void generateThreetoFour() {
         PrimeGenerator gen = new PrimeGenerator(3,4);
-        LinkedList<Long> expected= new LinkedList<Long>(Arrays.asList(3L));
+        Long[] expected={3L};
         gen.generatePrimes();
-        LinkedList<Long> actual = gen.getPrimes();
-        assertEquals(expected, actual);
+        assertArrayEquals(expected, gen.getPrimes().toArray());
     }
     @Test
     public void generatePrimesOnetoTwenty() {
-        PrimeGenerator gen = new PrimeGenerator(1, 20);
-        LinkedList<Long> expected= new LinkedList<Long>(Arrays.asList(2L,3L,5L,7L,11L,13L,17L,19L));
+        PrimeGenerator gen = new PrimeGenerator(1,20);
+        Long[] expected={2L,3L,5L,7L,11L,13L,17L,19L};
         gen.generatePrimes();
-        LinkedList<Long> actual = gen.getPrimes();
-        assertEquals(expected, actual);
+        assertArrayEquals(expected, gen.getPrimes().toArray());
     }
 }
