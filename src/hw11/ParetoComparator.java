a8d60b1e52eb7d3e3096f25c1db3bd38eaec3249
jameshardenfan5 <ajg2157@caa.columbia.edu>
2020-12-15 18:40:10 -0500

    Add files via upload

---
+++ src/hw11/ParetoComparator.java
@@ -0,0 +1,10 @@
+package hw11;
+import java.util.Comparator;
+
+public class ParetoComparator implements Comparator<Car>{
+
+    public int compare(Car car1,Car car2){
+
+        return car1.getdomcount()-car2.getdomcount();
+    }
+}
---
+++ src/hw11/YearComparator.java
@@ -0,0 +1,10 @@
+package hw11;
+import java.util.Comparator;
+
+public class YearComparator implements Comparator<Car>{
+
+    public int compare(Car car1,Car car2){
+
+        return car2.getYear()-car1.getYear();
+    }
+}
