2633dd09292eb0a71e9930c6555d9fc1fdd74ee1
jameshardenfan5 <ajg2157@caa.columbia.edu>
2020-12-19 13:26:11 -0500

    Update Car.java

--- src/hw11/Car.java
+++ src/hw11/Car.java
@@ -8,7 +8,7 @@ public class Car{
 	private int year, mileage,domcount=0;
 	private float price;
 
-	public Car(float price, int year, int mileage, String make,String model) {
+	public Car(float price, int year, int mileage, String make,String model){
 
 		this.price = price;
 		this.year = year;
@@ -17,37 +17,47 @@ public class Car{
 		this.make=make;
 	}
 	
-	public float getPrice() {
+	public float getPrice(){
 
 		return price;
 	}
 
-	public int getYear() {
+	public int getYear(){
 
 		return year;
 	}
 
-	public int getMileage() {
+	public int getMileage(){
 
 		return mileage;
 	}
 
-	public String getMake() {
+	public String getMake(){
 
 		return make;
 	}	
 
-	public String getModel() {
+	public String getModel(){
 
 		return model;
 	}
+	
     public void setdomcount(ArrayList<Car> cars){
 
         for(int i=0;i<cars.size();i++){
 
-            if(this.year <= cars.get(i).getYear() && this.price >= cars.get(i).getPrice() && this.mileage >= cars.get(i).getMileage() && (this.year < cars.get(i).getYear() || this.price > cars.get(i).getPrice() || this.mileage > cars.get(i).getMileage())){
+			int bool = (this.year != cars.get(i).getYear() || this.price != cars.get(i).getPrice() || this.mileage != cars.get(i).getMileage()) ? 1 : 0;
 
-                    this.domcount++;
+            switch(bool){
+				case 1:
+					if(this.price >= cars.get(i).getPrice()){ 
+						if(this.mileage >= cars.get(i).getMileage()){
+							if(this.year <= cars.get(i).getYear()){
+								this.domcount++;
+							}
+						}
+					}
+					break;
             }
         }
     }
