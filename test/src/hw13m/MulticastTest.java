91dd0d1090a217a445be124c5c96a0d38a6711b9
jameshardenfan5 <ajg2157@caa.columbia.edu>
2020-12-20 17:31:44 -0500

    Update MulticastTest.java

--- test/src/hw13m/MulticastTest.java
+++ test/src/hw13m/MulticastTest.java
@@ -2,28 +2,51 @@ package hw13m;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import java.time.LocalDateTime;
-import org.junit.jupiter.api.BeforeAll;
-import java.util.LinkedList;
-//import hw12.fs.*;
 
 public class MulticastTest{
 
 	@Test
 	public void equityTest(){
-
         StockQuoteObservable s = new StockQuoteObservable();
 		TableObserver t= new TableObserver();
-		//StockEvent e=new StockEvent("ASDF", 18.69f);
-
+		PieChartObserver p=new PieChartObserver();
+		ThreeDimensionObserver d=new ThreeDimensionObserver();
 
         s.addObserver(t);
-		//add 3 observers
-
+        s.addObserver(p);
+        s.addObserver(d);
         s.notifyObservers(new StockEvent("ASDF", 18.69f));
-
         assertEquals("ASDF", t.tabledisplayticker());
-		//test all observers
+        assertEquals("ASDF", p.piedisplayticker());
+        assertEquals("ASDF", d.threedimdisplayticker());
+        assertEquals(18.69f, t.tabledisplayquote());
+        assertEquals(18.69f, p.piedisplayquote());
+        assertEquals(18.69f, d.threedimdisplayquote());
+        s.notifyObservers(new StockEvent("FDSA", 1.695f));
+        assertEquals("FDSA", t.tabledisplayticker());
+        assertEquals("FDSA", p.piedisplayticker());
+        assertEquals("FDSA", d.threedimdisplayticker());
+        assertEquals(1.695f, t.tabledisplayquote());
+        assertEquals(1.695f, p.piedisplayquote());
+        assertEquals(1.695f, d.threedimdisplayquote());
 	}
+	@Test
+	public void dowjonesTest(){
+        DJIAQuoteObservable j = new DJIAQuoteObservable();
+		TableObserver t= new TableObserver();
+		PieChartObserver p=new PieChartObserver();
+		ThreeDimensionObserver d=new ThreeDimensionObserver();
 
+        j.addObserver(t);
+        j.addObserver(p);
+        j.addObserver(d);
+        j.notifyObservers(new DJIAEvent(29646.4f));
+        assertEquals(29646.4f, t.tabledisplaydjia());
+        assertEquals(29646.4f, p.piedisplaydjia());
+        assertEquals(29646.4f, d.threedimdisplaydjia());
+        j.notifyObservers(new DJIAEvent(29646.39f));
+        assertEquals(29646.39f, t.tabledisplaydjia());
+        assertEquals(29646.39f, p.piedisplaydjia());
+        assertEquals(29646.39f, d.threedimdisplaydjia());
+	}
 }
