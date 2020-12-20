34a0690f5148d279dc9cd49c7edbd2f04f0a1f83
jameshardenfan5 <ajg2157@caa.columbia.edu>
2020-12-20 18:21:22 -0500

    Add files via upload

---
+++ test/src/hw10/apfs/APFSTest.java
@@ -0,0 +1,16 @@
+package hw10.apfs;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import hw10.fs.*;
+
+public class APFSTest{
+
+    @Test
+    public void initDefaultRoot(){
+        String[] expected = {"true","root","0", "0","0"};
+        String[] actual={String.valueOf(APFS.getInstance().getRootDir().isDirectory()),APFS.getInstance().getRootDir().getName(),Integer.toString(APFS.getInstance().getRootDir().getSize()),Integer.toString(APFS.getInstance().getRootDir().countChildren()),Integer.toString(APFS.getInstance().getRootDir().getTotalSize())};
+
+        assertArrayEquals(expected,actual);
+    }
+}
