df334e63f63ef39948a26829e5b84acbba6e1d98
jameshardenfan5 <ajg2157@caa.columbia.edu>
2020-12-20 18:16:55 -0500

    Update APFSTest.java

--- test/src/hw09/apfs/APFSTest.java
+++ test/src/hw09/apfs/APFSTest.java
@@ -8,7 +8,6 @@ public class APFSTest{
 
     @Test
     public void initDefaultRoot(){
-
         String[] expected = {"true","root","0", "0","0"};
         String[] actual={String.valueOf(APFS.getInstance().getRootDir().isDirectory()),APFS.getInstance().getRootDir().getName(),Integer.toString(APFS.getInstance().getRootDir().getSize()),Integer.toString(APFS.getInstance().getRootDir().countChildren()),Integer.toString(APFS.getInstance().getRootDir().getTotalSize())};
 
